39544064d1df4219c521267855556713b6bfc257
Hunter_Ehrenfeld <hje61500@ucmo.edu>
2021-02-23 09:18:17 -0600

    Added alert model and controller

---
+++ src/main/java/edu/ucmo/commerce/controller/AlertController.java
@@ -0,0 +1,37 @@
+package edu.ucmo.commerce.controller;
+
+import edu.ucmo.commerce.dao.AlertDao;
+import edu.ucmo.commerce.model.Alert;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("alerts")
+public class AlertController {
+    @Autowired
+    private AlertDao alertDao;
+
+    @PostMapping
+    public Alert saveAlert(@RequestBody Alert alert){
+        Alert newAlert = new Alert(
+                new Timestamp(System.currentTimeMillis()),
+                alert.getHostname(),
+                alert.getApplication_id(),
+                alert.getFile(),
+                alert.getChange_agent(),
+                alert.getChange_process()
+                );
+        return alertDao.save(newAlert);
+    }
+
+    @GetMapping
+    public List<Alert> listAlerts(){
+        List<Alert> list = new ArrayList();
+        alertDao.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+}
---
+++ src/main/java/edu/ucmo/commerce/model/Alert.java
@@ -0,0 +1,112 @@
+package edu.ucmo.commerce.model;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "alert")
+public class Alert {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private Timestamp timestamp;
+    @Column
+    private String hostname;
+    @Column
+    private int application_id;
+    @Column
+    private String file;
+    @Column
+    private String change_agent;
+    @Column
+    private String change_process;
+
+    public Alert(
+            Timestamp timestamp,
+            String hostname,
+            int application_id,
+            String file,
+            String change_agent,
+            String change_process) {
+        this.timestamp = timestamp;
+        this.hostname = hostname;
+        this.application_id = application_id;
+        this.file = file;
+        this.change_agent = change_agent;
+        this.change_process = change_process;
+    }
+    public Alert(){
+        this.timestamp = new Timestamp(System.currentTimeMillis());
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Timestamp getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Timestamp timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getHostname() {
+        return hostname;
+    }
+
+    public void setHostname(String hostname) {
+        this.hostname = hostname;
+    }
+
+    public int getApplication_id() {
+        return application_id;
+    }
+
+    public void setApplication_id(int application_id) {
+        this.application_id = application_id;
+    }
+
+    public String getFile() {
+        return file;
+    }
+
+    public void setFile(String file) {
+        this.file = file;
+    }
+
+    public String getChange_agent() {
+        return change_agent;
+    }
+
+    public void setChange_agent(String change_agent) {
+        this.change_agent = change_agent;
+    }
+
+    public String getChange_process() {
+        return change_process;
+    }
+
+    public void setChange_process(String change_process) {
+        this.change_process = change_process;
+    }
+
+    @Override
+    public String toString() {
+        return "Alert{" +
+                "id=" + id +
+                ", timestamp=" + timestamp +
+                ", hostname='" + hostname + '\'' +
+                ", application_id=" + application_id +
+                ", file='" + file + '\'' +
+                ", change_agent='" + change_agent + '\'' +
+                ", change_process='" + change_process + '\'' +
+                '}';
+    }
+}
