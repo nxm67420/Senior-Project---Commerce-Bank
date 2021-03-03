7927311caf78e9549cc881b0f59cb39f9fa22f65
Hunter_Ehrenfeld <hje61500@ucmo.edu>
2021-03-02 19:18:18 -0600

    Added security functionality

---
+++ src/main/java/edu/ucmo/commerce/configuration/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package edu.ucmo.commerce.configuration;
+
+import edu.ucmo.commerce.service.MyUserDetalisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    private MyUserDetalisService userDetalisService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth
+                .userDetailsService(userDetalisService)
+                .passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+                .authorizeRequests()
+                .antMatchers("/").hasAnyAuthority("USER","ADMIN")
+                .antMatchers("/alerts").hasAnyAuthority("USER","ADMIN")
+                .antMatchers(HttpMethod.POST, "/alerts").hasAnyAuthority("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/alerts/**").hasAnyAuthority("USER","ADMIN")
+                .antMatchers("/users").hasAnyAuthority("ADMIN")
+                .and()
+                .formLogin()
+                .usernameParameter("user_name")
+                .passwordParameter("password")
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID")
+                .permitAll();
+        http
+                .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+    }
+}
---
+++ src/main/java/edu/ucmo/commerce/service/MyUserDetalisService.java
@@ -0,0 +1,42 @@
+package edu.ucmo.commerce.service;
+
+import edu.ucmo.commerce.dao.UserDao;
+import edu.ucmo.commerce.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class MyUserDetalisService implements UserDetailsService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String userName){
+        User user = userDao.findByUserName(userName);
+        if(user == null){
+            throw new UsernameNotFoundException(String.format("The username %s doesn't exist", userName));
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        if(user.getRole().equals("USER")){
+            authorities.add(new SimpleGrantedAuthority("USER"));
+        } else{
+            authorities.add(new SimpleGrantedAuthority("ADMIN"));
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUserName(), user.getPassword(), true, true, true, true, authorities);
+    }
+
+}
