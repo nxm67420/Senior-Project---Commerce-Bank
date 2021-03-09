8e186aa9ad4bb23884fdd7a51ce5dda977b7b375
nxm67420 <46074688+nxm67420@users.noreply.github.com>
2021-03-09 15:02:17 -0600

    Merge branch 'pageIndexBranch' of /Users/nicholasmoore/Desktop/Spring21_Commerce with conflicts.

--- src/main/js/app.js
+++ src/main/js/app.js
@@ -1,13 +1,34 @@
+//Import React && Libraries
+import React from 'react';
+import {BrowserRouter as Router, Route, Swicth} from "react-router-dom";
+
+//Import React && DOM
 const React = require('react');
 const ReactDOM = require('react-dom');
 
+//Import Component Files Below
+import ListComponent from "./components/ListComponent";
+
 function App() {
     return (
-        <div>
-
+        <div className={}>
+            <Router>
+                <div>
+                    <h1 className={} style={style}> File Management</h1>
+                    <Switch>
+                        <!--Insert Components-->
+                        <Route path={"/"} exact component={}/>
+                        <Route path={""} component={}></Route>
+                    </Switch>
+                </div>
+            </Router>
         </div>
     );
 }
+
+const style = {
+    color:aqua
+};
 
 export default App;
 
--- src/main/js/services/ApiService.js
+++ src/main/js/services/ApiService.js
@@ -1,6 +1,10 @@
+//'Axios' Handles our GET/POST/DELETE Request through API
+//Connect To our Dao Controllers
 import axios from 'axios';
 
-const CAR_API_BASE_URL = 'http://localhost:8080/alerts';
+//Base URL Link to Retrieve Data
+//const CAR_API_BASE_URL = 'http://localhost:8080/alerts';
+const ALERT_API_BASE_URL = 'http://localhost:8080/alerts';
 
 const CSRF_TOKEN = document.cookie.match(new RegExp(`XSRF-TOKEN=([^;]+)`))[1];
 const instance = axios.create({
@@ -8,7 +12,34 @@ const instance = axios.create({
 });
 
 class ApiService{
+    /* Methods GET/POST/ DELETE etc... */
+
+    //Fetches ALL Alerts
+    //Maybe Replacing ALL instance.get into axios.get ???
     fetchAlerts(){
-        return instance.get(CAR_API_BASE_URL)
+        return instance.get(ALERT_API_BASE_URL)
+    }
+
+    //Fetches Alerts according to Specific ID
+    fetchAlertById(alertId){
+        return instance.get(ALERT_API_BASE_URL + '/' + alertId);
+    }
+
+    //Deletes Alert according to Specific ID
+    deleteAlert(alertId){
+        return instance.delete(ALERT_API_BASE_URL + '/' + alertId);
+    }
+
+    //Add File /* Not essential just yet*/
+    //Will only be accessible through Admin, Higher Privilege
+    //Will Assign File to specific users
+    addAlert(alert){
+        return instance.post("" + ALERT_API_BASE_URL, alert)
+    }
+
+
+    //Edit Specific File according to Specific ID || Application ID
+    editAlert(alert){
+        return instance.put(ALERT_API_BASE_URL + '/' + alert.id, alert);
     }
 }
