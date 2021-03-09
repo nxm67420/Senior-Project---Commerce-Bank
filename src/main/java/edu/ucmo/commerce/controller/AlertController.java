020a5f6798b48b089465c24182eda7767737bfdf
nxm67420 <46074688+nxm67420@users.noreply.github.com>
2021-03-08 21:33:48 -0600

    Added To AlertController Added To AlertDao Commented Lines of Code Altered Alert_Table (Column : checked Boolean Not Null)<->(checked Boolean Not Null Default 0)

--- src/main/java/edu/ucmo/commerce/controller/AlertController.java
+++ src/main/java/edu/ucmo/commerce/controller/AlertController.java
@@ -3,16 +3,18 @@ package edu.ucmo.commerce.controller;
 import edu.ucmo.commerce.dao.AlertDao;
 import edu.ucmo.commerce.model.Alert;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.bind.annotation.*;
 
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-@RestController
+
+@RestController /* Communicates our API React(Javascript) <----> SpringBoot Backend */
 @RequestMapping("alerts")
 public class AlertController {
+
     @Autowired
     private AlertDao alertDao;
 
@@ -29,10 +31,48 @@ public class AlertController {
         return alertDao.save(newAlert);
     }
 
+    //Return ALL Alerts
     @GetMapping
     public List<Alert> listAlerts(){
         List<Alert> list = new ArrayList<>();
-        alertDao.findAll().iterator().forEachRemaining(list::add);
+        alertDao.findAllBy().iterator().forEachRemaining(list::add);
         return list;
     }
+
+    //Return Alert w/ Specific ID
+    //Working
+    @GetMapping(value = "alerts/{id}")
+    public Alert returnOne(@PathVariable int id){
+        Optional<Alert> optionalAlert = alertDao.findById(id); //If exist in the Database
+        return optionalAlert.isPresent() ? optionalAlert.get() : null; //THEN Return
+    }
+
+    //Return (Alerts.Checked == False)
+    //Working
+    @GetMapping(value = "/checking/{checked}")
+    public List<Alert> notChecked(@PathVariable boolean checked){
+     //   Optional<Alert> unchecked = alertDao.findByChecked(checked); //If exist in the Database
+     //   return unchecked.isPresent() ? unchecked.get() : null; //THEN Return
+        List<Alert> list = new ArrayList<>();
+        alertDao.findByChecked(checked).iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+
+    //Return (Alerts Checked == True)
+    //In Progress
+
+
+    //Return (Alerts.Checked == False && Days Passed > 2)
+    //DaysPassed - Posted Date
+    //In Progress
+    /*@GetMapping("checked/{checked}/days/{days}")
+    public Alert passedDue(Integer checked, Integer days){
+        List<Alert> list = new ArrayList<>();
+        alertDao.findByCheckedAndTimestamp(0, 2).iterator().forEachRemaining(list::add);
+        return (Alert)list;
+    }*/
+
+    //Save Altered Process
+    //Admin Name, Timestamp, Comments
 }
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -7,18 +7,18 @@ CREATE TABLE alert (
   file VARCHAR(255) NOT NULL,
   hostname VARCHAR(255) NOT NULL,
   timestamp TIMESTAMP NOT NULL,
-  checked BOOLEAN NOT NULL,
+  checked BOOLEAN NOT NULL DEFAULT 0,
   PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
-  user_id INT    NOT NULL AUTO_INCREMENT,
-  user_name VARCHAR(255) NOT NULL,
-  email VARCHAR(320) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  first_name Varchar(50) NOT NULL,
-  last_name  VARCHAR(50) NOT NULL,
-  role VARCHAR(255) NOT NULL,
-  PRIMARY KEY (user_id)
+user_id INT    NOT NULL AUTO_INCREMENT,
+user_name VARCHAR(255) NOT NULL,
+email VARCHAR(320) NOT NULL,
+password VARCHAR(255) NOT NULL,
+first_name Varchar(50) NOT NULL,
+last_name  VARCHAR(50) NOT NULL,
+role VARCHAR(255) NOT NULL,
+PRIMARY KEY (user_id)
 );
